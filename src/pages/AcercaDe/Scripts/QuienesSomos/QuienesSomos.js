369d0fe7a12fac776df9f381b492d193f830b3b0
Antonio Goeminne <antonionicolas3003@gmail.com>
2021-10-02 18:48:37 -0300

    terminada pagina acercaDe

--- src/pages/AcercaDe/Scripts/QuienesSomos/QuienesSomos.js
+++ src/pages/AcercaDe/Scripts/QuienesSomos/QuienesSomos.js
@@ -9,23 +9,23 @@ export const QuienesSomos = () => {
        <Fragment>
        <div className='quienesSomos--wrap'>
            <h2>Quienes Somos</h2>
-           <img src='/img/carrousel/img_carrusel_1.webp'></img> 
+           <img src='/img/pages/donar/acercaDe/quienes-somos.jpg'></img> 
        </div>
        <div className='quienesAcompanan--wrap'>
            <h1 className='quienesAcompanan--title'>Quienes nos acompañan</h1>
            <div className='empresas'>
           <div className='empresa'>
-              <img src='/img/Png/mother.png'></img>
+              <img src='/img/pages/donar/acercaDe/ministerio.jpg'></img>
               <h4>Ministerio de desarrollo social</h4>
               <p>Secretaria de la niñez, adolescencia y Familia</p>
           </div>
           <div className='empresa'>
-              <img src='/img/Png/mother.png'></img>
+              <img className='sirius--image' src='/img/pages/donar/acercaDe/sirius.jpg'></img>
               <h4>Sirius</h4>
               <p>Soluciones de Software</p>
           </div>
           <div className='empresa' id='fundacionHorizonte'>
-              <img src='/img/Png/mother.png'></img>
+              <img className='horizonte--maxima_image' src='/img/pages/donar/acercaDe/horizonte-maxima.png'></img>
               <h4>Dynamai App es un emprendimiento de la fundación Horizonte de Máxima</h4>
               <p>fundacion</p>
           </div>
--- src/pages/AcercaDe/Scripts/TargetArticle/ArticleBloques.js
+++ src/pages/AcercaDe/Scripts/TargetArticle/ArticleBloques.js
@@ -3,7 +3,10 @@ export const ArticleBloques = () => {
     return(
         <div className='bloques--wrap'>
             <h3 className='bloques--title'>EDAD DE LAS MADRES</h3>
-            <img className='bloques--grafico1' src="/img/Png/Grafico-usuarios1.png"></img>
+            <div className='primer--bloque_wrap'>
+                <img className='bloques--grafico1' src="/img/Png/Grafico-usuarios1.png"></img>
+                <img className='bloques--mothers' src='/img/Png/Madres.png'></img>
+            </div>
             <h3 className='bloques--title2'>NIVEL EDUCATIVO DE MADRES PRIMERIZAS</h3>
                 <div className='bloques--png_wrap'>
                     <img className='bloques' src='/img/Png/bloques.png'></img>
