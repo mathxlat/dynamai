57aa67b01e76e166d53c4d2c75244f6271ffafd5
Antonio Goeminne <antonionicolas3003@gmail.com>
2021-11-01 18:57:34 -0300

    trabajando con telefono del formulario

--- src/components/forms/Formulario.js
+++ src/components/forms/Formulario.js
@@ -1,50 +1,75 @@
-import {Formik, Form} from 'formik'
+import { useState } from 'react'
+import {Formik, Form, useFormik} from 'formik'
 import { TextInput } from './TextInput'
+import PhoneInput from 'react-phone-number-input'
 import '../../pages/AcercaDe/styles/formulario/dist/formulario.css'
 
 
 function Formulario () {
+    const[formularioEnviado, setFormularioEnviado] = useState(false)
+    const[telefono, setTelefono] = useState()
+
+    const formik = useFormik({
+        initialValues: {
+            mensaje:''
+        }
+    })
+
     const validate = (values) =>{
         
             const errors = {}
 
             if(!values.institucion){
                 errors.institucion = '*'
-            }else if(values.institucion.length <3){
-                errors.institucion = 'debe escribir más de 3 digitos'
+            }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.institucion)){
+                errors.institucion = 'El nombre solo puede contener letras y espacios'
             }
             
             if(!values.contacto){
                 errors.contacto = '*'
-            }else if(values.contacto.length <3){
-                errors.contacto = 'debe escribir más de 5 digitos'
+            }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.contacto)){
+                errors.contacto = 'El nombre solo puede contener letras y espacios'
             }
 
             if(!values.email){
                 errors.email = '*'
-            }else if(values.email.length <5){
-                errors.email = 'debe escribir más de 5 digitos'
+            }else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)){
+                errors.email = "Se debe respetar la estructura de un e-mail"
+            }
+            if(!values.telefono){
+                errors.telefono = '*'
+            }else if(!/^[0-9-]{1,10}$/.test(values.telefono)){
+                errors.telefono = 'Solo puedes usar numeros'
             }
             return errors;
     }
     return(
         <div className='formik--box'>
-    <Formik  initialValues={{institucion:'', contacto:'',email:'',telefono:'', mensaje:''}}
+             {formularioEnviado == false? 
+    <Formik  initialValues={{institucion:'', contacto:'',email:'',telefono:''}}
             validate={validate}
-            onSubmit={values => console.log(values)}
+            onSubmit={(values, {resetForm}) => {
+                resetForm();
+                console.log(values);
+                console.log(formik.values.mensaje);
+                setFormularioEnviado(true);
+            }}
     >   
-       <Form className='main--form'>
-          <TextInput name='institucion' label='institucion' placeholder='Benjamin SRL'/>
+     <Form className='main--form'>
+          <TextInput name='institucion' label='nombre de la institucion' placeholder='Benjamin SRL'/>
           <TextInput name='contacto' label='contacto' placeholder='Benjamin Nievas'/>
-          <TextInput name='telefono' label='telefono' placeholder='1135224246'/>
+          <PhoneInput name='telefono' placeholder='ingrese su numero' onChange={setTelefono} value={telefono} countryCallingCodeEditable={false}  defaultCountry="RU"
+            />
           <TextInput name='email' label='email' placeholder='benjaminnievas@gmail.com'/>
           <div className='optional--box'>
             <label className='optional--label'>mensaje opcional</label>
-            <input className='optional--message' />
+            <input className='optional--message'  name='mensaje' value={formik.values.mensaje} onChange={formik.handleChange}/>
             <button className='main--form_btn' type='submit'>ENVIAR</button>
           </div>
        </Form>
-    </Formik>
+     </Formik> 
+: <div>enviado¡</div>
+        }
     </div>
     )
 }
--- src/components/forms/TextInput.js
+++ src/components/forms/TextInput.js
@@ -10,7 +10,7 @@ export const TextInput = ({ placeholder, label, ...props}) => {
             <label className='input--box_label'>{label}</label>
                 <input  className='input--box_input' {...field} placeholder={placeholder}/>
                 {
-                    meta.touched && meta.error ? <div>{meta.error}</div> : null
+                    meta.touched && meta.error ? <div className='error--message'>{meta.error}</div> : null
                 }
         </div>
     )
